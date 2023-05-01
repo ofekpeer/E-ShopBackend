6527a1486f231ff9c5e5da86c4669f2ec0d18e27
ofek <ofekpeer3030@gmail.com>
2023-05-01 12:30:28 +0300

    terd commit

--- Frontend/frontend/src/App.js
+++ Frontend/frontend/src/App.js
@@ -1,12 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import data from './data';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <a href='/'>E-Shop</a>
+        <a href="/">E-Shop</a>
       </header>
+      <main>
+        <h1>Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div key={product.token} className='product'>
+              <img alt={product.name} src={product.image}></img>
+              <p>{product.name}</p>
+              <p>{product.price}</p>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
