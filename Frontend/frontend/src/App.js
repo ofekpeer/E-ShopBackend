62ffe75ecc992f4b49ab0259bb30b94b2cdd81f5
ofek <ofekpeer3030@gmail.com>
2023-05-04 02:12:55 +0300

    new commit

--- Frontend/frontend/src/App.js
+++ Frontend/frontend/src/App.js
@@ -1,21 +1,21 @@
-import {Routes, Route, BrowserRouter, Link} from 'react-router-dom'
+import { Routes, Route, BrowserRouter, Link } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ProductPage from './pages/ProductPage';
 
 function App() {
   return (
     <BrowserRouter>
-    <div className="App">
-      <header className="App-header">
-        <Link to="/">E-Shop</Link>
-      </header>
-      <main>
-        <Routes>
-          <Route path='/product/:token' element={<ProductPage/>}></Route>
-          <Route path='/' element={<HomePage/>} />
-        </Routes>
-      </main>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <Link to="/">E-Shop</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/product/:token" element={<ProductPage />}></Route>
+            <Route path="/" element={<HomePage />} />
+          </Routes>
+        </main>
+      </div>
     </BrowserRouter>
   );
 }
